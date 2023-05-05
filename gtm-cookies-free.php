f6659c0fc5e015a8ac4a7ac691d23d0c10724311
Michał Frankiewicz <michal@frankiewicz.me>
2023-05-05 14:33:58 +0200

    initial commit

---
+++ gtm-cookies-free.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: GTM Cookies Free
+ * Plugin URI:  https://wordpress.org/plugins/gtm-cookies-free
+ * Description: TODO_DESC
+ * Version:     0.0.1
+ * Author:      Tag Concierge
+ * Author URI:  https://tagconcierge.com/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: gtm-cookies-free
+ * Domain Path: /languages
+ */
+
+var_dump('hello from gtm cookies free');
