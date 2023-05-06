11411ae46634e3eebddcb9c118ce2ea239442385
Michał Frankiewicz <michal@frankiewicz.me>
2023-05-06 13:39:40 +0200

    build script

---
+++ dev/scripts/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+docker-compose run --rm -T php-cli <<INPUT
+composer install --no-dev
+
+rm -rf dist/
+mkdir -p dist/gtm-cookies-free
+
+cp gtm-cookies-free.php dist/gtm-cookies-free/
+cp -R src dist/gtm-cookies-free/src
+cp -R vendor dist/gtm-cookies-free/vendor
+cp -R composer.* dist/gtm-cookies-free/
+
+cd dist && zip -r gtm-ecommerce-woo.zip gtm-cookies-free
+
+INPUT
