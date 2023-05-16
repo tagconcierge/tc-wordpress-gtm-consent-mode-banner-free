6dc6296e8514e102ec56475c69136bcaaaa25f15
Michał Frankiewicz <michal@frankiewicz.me>
2023-05-16 17:52:52 +0200

    adds gtm loading

--- src/Service/GtmSnippetService.php
+++ src/Service/GtmSnippetService.php
@@ -24,8 +24,21 @@ class GtmSnippetService
         if ('1' === $this->settingsUtil->getOption('disabled')) {
             return;
         }
+
+        $deferEvents = '1' === $this->settingsUtil->getOption('defer_events') ? 'true' : 'false';
+        $consentEventName = $this->settingsUtil->getOption('consent_event_name') ? $this->settingsUtil->getOption('consent_event_name') : '';
+        $requireConsentBeforeGtmLoad = '1' === $this->settingsUtil->getOption('gtm_snippet_consent_required') ? 'true' : 'false';
+
         $script = <<<EOD
+window.dataLayer = window.dataLayer || [];
+
 window.gtmCookies = {
+    config: {
+        gtmLoaded: false,
+        deferEvents: {$deferEvents},
+        consentEventName: '{$consentEventName}',
+        requireConsentBeforeGtmLoad: {$requireConsentBeforeGtmLoad}
+    },
     deferredEvents: [],
     isConsentGranted: function () { return '1' === localStorage.getItem('GTM_COOKIES_CONSENT');},
     setConsent: function (isGranted) { return localStorage.setItem('GTM_COOKIES_CONSENT', true === isGranted ? '1' : '0'); },
@@ -42,54 +55,131 @@ window.gtmCookies = {
             return;
         }
         gtmCookies.callbacks[event].forEach(function(callback) { callback(data); });
+    },
+    grantConsent: function() {
+        dataLayer.push({'event': gtmCookies.config.consentEventName});
+        gtmCookies.setConsent(true);
+        gtmCookies.emit('consentGranted');
+    },
+    repushEvents: function() {
+        let events = [];
+        while (0 < gtmCookies.deferredEvents.length) {
+            events.push(gtmCookies.deferredEvents.pop());
+        }
+        
+        while (0 < events.length) {
+            dataLayer.originPush(events.pop());
+        }
     }
 };
 
-window.dataLayer = window.dataLayer || [];
+gtmCookies.on('gtmLoaded', function() {
+    gtmCookies.config.gtmLoaded = true;
+});
 EOD;
         $this->outputUtil->addInlineScript($script, false);
 
 
         if ('1' === $this->settingsUtil->getOption('defer_events')) {
-            $consentEventName = $this->settingsUtil->getOption('consent_event_name');
             $script = <<<EOD
 dataLayer.originPush = dataLayer.push;
 dataLayer.push = function(item) {
-    if (true === gtmCookies.isConsentGranted()) {
-        return dataLayer.originPush(item);
+    const isGtmObject = 'object' === typeof item && item.hasOwnProperty('event') && item['event'].includes('gtm.');
+    let result = null;
+    
+    if (true === gtmCookies.config.requireConsentBeforeGtmLoad && true === isGtmObject) {
+        result = dataLayer.originPush(item);
+    } else if (true === gtmCookies.config.requireConsentBeforeGtmLoad && false === gtmCookies.config.gtmLoaded) {
+        result = gtmCookies.deferredEvents.push(item);
+    } else if (true === gtmCookies.isConsentGranted()) {
+        result = dataLayer.originPush(item);
+    } else {
+        result = gtmCookies.deferredEvents.push(item);
     }
-    
-    const result = gtmCookies.deferredEvents.push(item);
-    
-    if ('object' === typeof item) {
-        if (item.hasOwnProperty('event') && '{$consentEventName}' === item['event']) {
-            gtmCookies.emit('consentGranted');
-        }
+        
+    if (true === isGtmObject && 'gtm.load' === item['event']) {
+        gtmCookies.emit('gtmLoaded');
     }
     
     return result;
 };
 
-gtmCookies.on('consentGranted', function() {
-    gtmCookies.setConsent(true);
-    gtmCookies.deferredEvents.forEach(function(event){
-        dataLayer.originPush(event);
-    });
+gtmCookies.on('consentGranted', function() {    
+    if (true === gtmCookies.config.requireConsentBeforeGtmLoad) {
+        gtmCookies.on('gtmLoaded', function() {
+            gtmCookies.repushEvents();
+        });
+    } else {
+        gtmCookies.repushEvents();
+    }
 });
+
+if (true === gtmCookies.config.requireConsentBeforeGtmLoad) {
+    gtmCookies.on('gtmLoaded', function() {
+        gtmCookies.repushEvents();
+    });
+}
 EOD;
             $this->outputUtil->addInlineScript($script, false);
         }
 
-        if (false === empty($this->settingsUtil->getOption('gtm_snippet_head'))) {
-            add_action( 'wp_head', [$this, 'headSnippet'], 0 );
+        $requireConsentBeforeGtmLoad = '1' === $this->settingsUtil->getOption('gtm_snippet_consent_required');
+
+        if (false === empty($headSnippet = $this->settingsUtil->getOption('gtm_snippet_head'))) {
+            if (true === $requireConsentBeforeGtmLoad) {
+                $gtmId = $this->extractGtmId($headSnippet);
+                $script = <<<EOD
+const gtmCookiesLoadHeadSnippet = function() {
+    if (false === gtmCookies.isConsentGranted()) {
+        setTimeout(gtmCookiesLoadHeadSnippet, 1000);
+        return;
+    }
+    
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','{$gtmId}');
+};
+
+gtmCookiesLoadHeadSnippet();
+EOD;
+                $this->outputUtil->addInlineScript($script);
+            } else {
+                add_action( 'wp_head', [$this, 'headSnippet'], 0 );
+            }
         }
 
-        if (false === empty($this->settingsUtil->getOption('gtm_snippet_body'))) {
-            add_action( 'wp_body_open', [$this, 'bodySnippet'], 0 );
+        if (false === empty($bodySnippet = $this->settingsUtil->getOption('gtm_snippet_body'))) {
+            if (true === $requireConsentBeforeGtmLoad) {
+                $gtmId = $this->extractGtmId($bodySnippet);
+                $script = <<<EOD
+const gtmCookiesLoadBodySnippet = function() {
+    if (false === gtmCookies.isConsentGranted()) {
+        setTimeout(gtmCookiesLoadBodySnippet, 1000);
+        return;
+    }
+
+    const noscript = document.createElement('noscript');
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'https://www.googletagmanager.com/ns.html?id={$gtmId}');
+    iframe.setAttribute('height', '0');
+    iframe.setAttribute('width', '0');
+    iframe.setAttribute('style', 'display:none;');
+    noscript.appendChild(iframe);
+    document.body.appendChild(noscript);
+};
+
+gtmCookiesLoadBodySnippet();
+EOD;
+                $this->outputUtil->addInlineScript($script);
+            } else {
+                add_action( 'wp_body_open', [$this, 'bodySnippet'], 0 );
+            }
         }
     }
 
-    public function headSnippet(): void
+    public function headSnippet($headSnippet): void
     {
         echo filter_var($this->settingsUtil->getOption('gtm_snippet_head'), FILTER_FLAG_STRIP_BACKTICK) . "\n";
     }
@@ -97,5 +187,12 @@ EOD;
     public function bodySnippet(): void
     {
         echo filter_var($this->settingsUtil->getOption('gtm_snippet_body'), FILTER_FLAG_STRIP_BACKTICK) . "\n";
+    }
+
+    private function extractGtmId(string $string): string
+    {
+        preg_match('/GTM-[a-zA-Z0-9]+/', $string, $matches);
+
+        return false === empty($matches) ? $matches[0] : '';
     }
 }
--- src/Util/OutputUtil.php
+++ src/Util/OutputUtil.php
@@ -9,6 +9,7 @@ class OutputUtil
     public function __construct()
     {
         add_action( 'wp_head', [$this, 'wpHead'], 0 );
+        add_action( 'wp_footer', [$this, 'wpFooter'], 0 );
     }
 
     public function addInlineScript($script, $footer = true): OutputUtil
@@ -21,11 +22,24 @@ class OutputUtil
     public function wpHead(): void
     {
         if (0 === count($this->inlineScripts['header'])) {
-            echo '<!-- gtm-ecommerce-woo no-header-scripts -->';
+            echo '<!-- gtm-cookies no-header-scripts -->';
             return;
         }
         echo '<script type="text/javascript" data-gtm-cookies-scripts>';
         foreach ($this->inlineScripts['header'] as $script) {
+            echo filter_var($script, FILTER_FLAG_STRIP_BACKTICK) . "\n";
+        }
+        echo "</script>\n";
+    }
+
+    public function wpFooter(): void
+    {
+        if (0 === count($this->inlineScripts['footer'])) {
+            echo '<!-- gtm-cookies no-footer-scripts -->';
+            return;
+        }
+        echo '<script type="text/javascript" data-gtm-cookies-scripts>';
+        foreach ($this->inlineScripts['footer'] as $script) {
             echo filter_var($script, FILTER_FLAG_STRIP_BACKTICK) . "\n";
         }
         echo "</script>\n";
